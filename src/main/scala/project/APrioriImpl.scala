cbb39a00e2a53d08f2568950550430cc99d4817d
Bagus Trihatmaja <astungkara.project@gmail.com>
2019-03-24 15:41:14 -0400

    Project structure

---
+++ src/main/scala/project/APrioriImpl.scala
@@ -0,0 +1,22 @@
+package project
+
+import org.apache.log4j.LogManager
+import org.apache.spark.{SparkConf, SparkContext}
+
+object APrioriImpl {
+  def main(args: Array[String]): Unit = {
+    val logger: org.apache.log4j.Logger = LogManager.getRootLogger
+
+    if (args.length != 3) {
+      logger.error("Usage:\nproject.APrioriImpl <input> <output> <min-support>")
+    }
+
+    val conf = new SparkConf().setMaster("local")
+      .setAppName("Project - APriori")
+
+    val sc = new SparkContext(conf)
+    val input = args(0)
+    val output = args(1)
+    val minSupport: Float = Float(args(2))
+  }
+}
