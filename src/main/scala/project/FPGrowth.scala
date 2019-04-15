c3a26d4b035edc16658eb3738af793c2ee3e1e54
jayturakhia <turakhia.j@husky.neu.edu>
2019-04-15 19:24:13 -0400

    Added prints to locate mem overhead

--- src/main/scala/project/FPGrowth.scala
+++ src/main/scala/project/FPGrowth.scala
@@ -10,6 +10,7 @@ import org.apache.spark.mllib.fpm.FPGrowth
 object FPGrowth {
   def main(args: Array[String]): Unit = {
 
+    val t1= System.nanoTime
     val conf = new SparkConf().setMaster("local")
       .setAppName("Project - APriori")
 
@@ -19,11 +20,13 @@ object FPGrowth {
     val minSupport = args(2).toFloat
 
     val followeeList = input.map(x => (x.split("\t")(0), x.split("\t")(1).toInt)).groupByKey().map(f => f._2.toArray)
-    followeeList.collect().foreach(print)
+    println("\n\n\nFolloweeList Count- "+followeeList.count)
+    //followeeList.collect().foreach(print)
 
     val fpg = new FPGrowth()
     val model = fpg.setMinSupport(minSupport).run(followeeList)
 
     model.freqItemsets.saveAsTextFile(output)
+    println("\n\n\nTime = "+(System.nanoTime - t1)/1e9d)
   }
 }
