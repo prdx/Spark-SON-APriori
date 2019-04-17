0cb5532515caca2de51eb6e1086f52bac8de51b8
Bagus Trihatmaja <astungkara.project@gmail.com>
2019-04-17 16:28:14 -0400

    Update report and add repartition

--- src/main/scala/project/ProjectImpl.scala
+++ src/main/scala/project/ProjectImpl.scala
@@ -4,6 +4,7 @@ import org.apache.log4j.LogManager
 import org.apache.spark.{SparkConf, SparkContext}
 import algorithm.Apriori
 import algorithm.SONImpl
+
 object ProjectImpl {
   def main(args: Array[String]): Unit = {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
@@ -21,23 +22,21 @@ object ProjectImpl {
     val output = args(1)
     val minSupport = (args(2)).toFloat
     
-    //filtering by MaxID if required while testing. Commenting for now
-    /*val filteredInp = input.filter(x => (x.split("\t")(0)).toInt < 100 && (x.split("\t")(1)).toInt < 100)
-    filteredInp.foreach(x => println(x))*/
-    
-    //Generating followeeList as List(followee1, followee2)
-    val followeeList = input.map(x => (x.split("\t")(0),x.split("\t")(1).toInt)).groupByKey().map(y => (y._2.toList.sortBy(y => y)))
+    // filtering by MaxID if required while testing. Commenting for now
+    val filteredInp = input.filter(x => (x.split("\t")(0)).toInt < 100000 && (x.split("\t")(1)).toInt < 100000)
+
+    // Generating followeeList as List(followee1, followee2)
+    val followeeList = filteredInp.map(x => (x.split("\t")(0),x.split("\t")(1).toInt)).groupByKey().map(y => (y._2.toList.sortBy(y => y)))
+    followeeList.repartition(1000)
     println("\n\n\t\tFollowee List generated\n\n")
+
     var locals = followeeList.mapPartitions(partition => Apriori.execute(partition, minSupport)).map(x => (x,1)).reduceByKey(_+_).collect.toList
     println("\n\n\t\tLocals generated\n\n")
     //val myLocals = locals.
     //myLocals.foreach(x => println(x))
     val globals = followeeList.mapPartitions(partition => SONImpl.execute(partition, minSupport, locals)).reduceByKey(_+_)
     val minsp = followeeList.count * minSupport
-    globals.filter(x => x._2 >= minsp).sortBy(_._2, false).coalesce(1).saveAsTextFile(output)
-
-
-
+    globals.filter(x => x._2 >= minsp).sortBy(_._2, false).saveAsTextFile(output)
   }
   
 }
